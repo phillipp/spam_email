d9964c73450481cec7a3662561872ea279cdc974
motionless <henry.gallert@gmail.com>
2021-03-24 14:19:42 +0100

    Change the way of accessing the array to make it more robust

--- lib/spam_email/check.rb
+++ lib/spam_email/check.rb
@@ -9,7 +9,7 @@ module SpamEmail
 
     complete_domain = address.domain.downcase
     main_domain = PublicSuffix.domain(complete_domain)
-    mx_complete_domain = Resolv::DNS.new.getresources(complete_domain, Resolv::DNS::Resource::IN::MX)[0].exchange.to_s
+    mx_complete_domain = Resolv::DNS.new.getresources(complete_domain, Resolv::DNS::Resource::IN::MX).first&.exchange.to_s
     mx_main_domain = PublicSuffix.domain(mx_complete_domain)
 
     (BLACKLIST & [complete_domain, main_domain, mx_complete_domain, mx_main_domain]).any?
